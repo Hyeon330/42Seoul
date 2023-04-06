06df86c240847d8eb08d17b0700112f731602434
hyeon3 <dldlsrn231@gmail.com>
2023-04-07 04:09:08 +0900

    update

--- Cadet/codes/Circle02/so_long/src/main.c
+++ Cadet/codes/Circle02/so_long/src/main.c
@@ -35,6 +35,29 @@ int	set_map(char *mapname, t_vars *vars)
 	return (chk_map1(vars));
 }
 
+void	fill_background(t_vars *vars)
+{
+	t_img	back_img;
+	int		height;
+	int		width;
+	int		i;
+	int		j;
+
+	height = vars->x_height * BLOCK_HEIGHT;
+	width = vars->x_width * BLOCK_WIDTH;
+	back_img.img = mlx_new_image(vars->mlx, BLOCK_WIDTH, BLOCK_HEIGHT);
+	back_img.addr = mlx_get_data_addr(back_img.img, &back_img.bits_per_pixel, &back_img.line_length, &back_img.endian);
+	i = -1;
+	while (++i < height)
+	{
+		j = -1;
+		while (++j < width)
+			my_mlx_pixel_put(&back_img, j, i, SPACE_COLOR);
+	}
+	free(back_img.img);
+	free(back_img.addr);
+}
+
 void	set_img(t_vars *vars, t_img *img, char *texture_path)
 {
 	img->img = mlx_xpm_file_to_image(vars->mlx, texture_path, &img->width, &img->height);
@@ -43,34 +66,33 @@ void	set_img(t_vars *vars, t_img *img, char *texture_path)
 
 void	set_textures(t_vars *vars, t_img *element)
 {
-	set_img(vars, &element[1], "textures/wall.xpm");
-	set_img(vars, &element[2], "textures/collectible.xpm");
-	set_img(vars, &element[3], "textures/exit_c.xpm");
-	set_img(vars, &element[4], "textures/exit_o.xpm");
-	set_img(vars, &element[5], "textures/player_r.xpm");
-	set_img(vars, &element[6], "textures/player_l.xpm");
+	set_img(vars, &element[0], "textures/wall.xpm");
+	set_img(vars, &element[1], "textures/collectible.xpm");
+	set_img(vars, &element[2], "textures/exit_c.xpm");
+	set_img(vars, &element[3], "textures/exit_o.xpm");
+	set_img(vars, &element[4], "textures/player_r.xpm");
+	set_img(vars, &element[5], "textures/player_l.xpm");
 }
 
-// void	clear_img(t_img *element)
-// {
-	
-// }
+void	clear_img(t_img *element)
+{
+	int	i;
+
+	i = -1;
+	while (++i < 6)
+	{
+		free(element[i].img);
+		free(element[i].addr);
+	}
+}
 
 void	view(t_vars *vars)
 {
-	t_img	element[7];
-	int	i;
-	int	j;
+	t_img	element[6];
+	int		i;
+	int		j;
 
-	element[0].img = mlx_new_image(vars->mlx, BLOCK_WIDTH, BLOCK_HEIGHT);
-	element[0].addr = mlx_get_data_addr(element[0].img, &element[0].bits_per_pixel, &element[0].line_length, &element[0].endian);
-	i = -1;
-	while (++i < BLOCK_HEIGHT)
-	{
-		j = -1;
-		while (++j < BLOCK_WIDTH)
-			my_mlx_pixel_put(&element[0], j, i, SPACE_COLOR);
-	}
+	fill_background(vars);
 	set_textures(vars, element);
 	i = -1;
 	while (++i < vars->x_height)
@@ -79,26 +101,31 @@ void	view(t_vars *vars)
 		while (++j < vars->x_width)
 		{
 			if (vars->map[i][j] == '1')
-				mlx_put_image_to_window(vars->mlx, vars->win, element[1].img, j * BLOCK_WIDTH, i * BLOCK_HEIGHT);
+				mlx_put_image_to_window(vars->mlx, vars->win, element[0].img, j * BLOCK_WIDTH, i * BLOCK_HEIGHT);
 			if (vars->map[i][j] == 'C')
-				mlx_put_image_to_window(vars->mlx, vars->win, element[2].img, j * BLOCK_WIDTH, i * BLOCK_HEIGHT);
+				mlx_put_image_to_window(vars->mlx, vars->win, element[1].img, j * BLOCK_WIDTH, i * BLOCK_HEIGHT);
 			if (vars->map[i][j] == 'E')
-				mlx_put_image_to_window(vars->mlx, vars->win, element[3].img, j * BLOCK_WIDTH, i * BLOCK_HEIGHT);
+				mlx_put_image_to_window(vars->mlx, vars->win, element[2].img, j * BLOCK_WIDTH, i * BLOCK_HEIGHT);
 			if (vars->map[i][j] == 'P')
-				mlx_put_image_to_window(vars->mlx, vars->win, element[5].img, j * BLOCK_WIDTH, i * BLOCK_HEIGHT);
+				mlx_put_image_to_window(vars->mlx, vars->win, element[4].img, j * BLOCK_WIDTH, i * BLOCK_HEIGHT);
 		}
 	}
+	clear_img(element);
 }
 
 int	key_press(int keycode, t_vars *vars)
 {
-	ft_printf("%d\n", keycode);
+	static int	move;
+
 	if (keycode == 53)
-	{
-		mlx_destroy_window(vars->mlx, vars->win);
-		exit(EXIT_SUCCESS);
-	}
+		close(vars);
 	return (0);
+}
+
+int	close(t_vars *vars)
+{
+	mlx_destroy_window(vars->mlx, vars->win);
+	exit(EXIT_SUCCESS);
 }
 
 int	main(int ac, char **av)
@@ -116,7 +143,8 @@ int	main(int ac, char **av)
 
 	fill_background();
 	view(&vars);
-	// mlx_hook(vars.win, 2, 1L<<0, key_press, &vars);
+	mlx_hook(vars.win, 2, 1L<<0, key_press, &vars);
+	mlx_hook(vars.win, 17, 1L<<0, close, &vars);
 	
 	mlx_loop(vars.mlx);
 }
