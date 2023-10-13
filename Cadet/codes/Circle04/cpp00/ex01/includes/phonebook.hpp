63c7607abb12778f974727f768775f62cfa1c30e
Hyeonsu Lee <hyeonsul@c4r6s8.42seoul.kr>
2023-10-13 17:45:15 +0900

    update

---
+++ Cadet/codes/Circle04/cpp00/ex01/includes/phonebook.hpp
@@ -0,0 +1,39 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+class PhoneBook
+{
+private:
+    Contact[8];
+public:
+    PhoneBook(/* args */);
+    ~PhoneBook();
+};
+
+PhoneBook::PhoneBook(/* args */)
+{
+}
+
+PhoneBook::~PhoneBook()
+{
+}
+
+class Contact
+{
+private:
+    /* data */
+public:
+    Contact(/* args */);
+    ~Contact();
+};
+
+Contact::Contact(/* args */)
+{
+}
+
+Contact::~Contact()
+{
+}
+
+
+#endif
---
+++ Cadet/codes/Circle04/cpp00/ex01/srcs/megaphone.cpp
@@ -0,0 +1,17 @@
+#include "megaphone.hpp"
+
+int main(int ac, char **av) {
+	int	i, j;
+
+	if (ac < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	i = 0;
+	while (++i < ac) {
+		j = -1;
+		while (av[i][++j])
+			av[i][j] = std::toupper(av[i][j]);
+		std::cout << av[i];
+	}
+	std::cout << std::endl;
+    return 0;
+}
