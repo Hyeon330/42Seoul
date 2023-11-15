e4c2754329bcc24233e7ddde222b0766db55065b
Hyeonsu Lee <hyeonsul@c6r4s5.42seoul.kr>
2023-11-15 22:27:31 +0900

    exam4 prac

--- exam4/microshell/microshell.c
+++ exam4/microshell/microshell.c
@@ -9,70 +9,142 @@
 
 typedef struct	s_cmd {
 	struct s_cmd	*next;
-	char			**av;
 	int				ac;
+	char			**av;
 }	t_cmd;
 
 typedef struct	s_token {
 	t_cmd	*cmd;
+	char	**env;
 	int		size;
 }	t_token;
 
-void	parse(int ac, char **av, t_token* token) {
-	
+int	cnt_av(char** av) {
+	int i;
+
+	i = -1;
+	while (av[++i] && strncmp(av[i], ";", 2) && strncmp(av[i], "|", 2)) ;
+	return i;
 }
 
-int	chk_pipe_col(char* str, int *fd) {
-	if (!strncmp(str, "|", 2)) {
-		pipe(fd);
-		return 1;
+void	set_av(char** av, t_cmd* cmd) {
+	int i;
+
+	i = -1;
+	while (++i < cmd->ac)
+		cmd->av[i] = av[i];
+}
+
+int	parse(char** av, char** env, t_token* token) {
+	t_cmd	*tail;
+	int		i;
+
+	tail = NULL;
+	i = 0;
+	while (av[i]) {
+		if (!strncmp(av[i], ";", 2))
+			break;
+		if (!strncmp(av[i], "|", 2))
+			i++;
+
+		if (!tail) {
+			token->cmd = (t_cmd*)malloc(sizeof(t_cmd));
+			tail = token->cmd;
+		} else {
+			tail->next = (t_cmd*)malloc(sizeof(t_cmd));
+			tail = tail->next;
+		}
+
+		memset(tail, 0, sizeof(t_cmd));
+		tail->ac = cnt_av(av + i);
+		tail->av = (char**)malloc(sizeof(char*) * (tail->ac + 1));
+		tail->av[tail->ac] = NULL;
+		set_av(av + i, tail);
+		token->size++;
+		i += tail->ac;
 	}
-	if (!strncmp(str, ";", 2))
-		return 1;
-	return 0;
+	token->env = env;
+	return i;
 }
 
-int main(int ac, char **av, char **env) {
-	t_cmd	cmd;
+char*	get_home(char** env) {
+	while (*env) {
+		if (!strncmp("HOME=", *env, 5))
+			return *env + 5;
+		env++;
+	}
+	return NULL;
+}
+
+void	cd(t_cmd* cmd, char** env) {
+	if (cmd->ac > 2) {
+		write(2, "error: cd: bad arguments\n", 25);
+		return ;
+	}
+	if (cmd->ac == 1) {
+		printf("%s\n", get_home(env));
+		chdir(get_home(env));
+		return ;
+	}
+	if (chdir(cmd->av[1]) < 0) 
+		write(2, "error: cd: cannot change directory to path_to_change\n", 53);
+}
+
+void	exec(t_token *token) {
+	t_cmd	*cmd;
 	pid_t	pid;
 	int		fd[2];
+	int		i;
 
-	dup2(STDIN_BACK, STDIN_FILENO);
-	dup2(STDOUT_BACK, STDOUT_FILENO);
-	dup2(STDERR_BACK, STDERR_FILENO);
-	while (*++av) {
-		memset(&cmd, 0, sizeof(t_cmd));
-		memset(fd, 0, sizeof(int) * 2);
-		if (chk_pipe_col(*av, fd)) {
-			if (!cmd.av)
-				exit(EXIT_FAILURE);
-			
-			pid = fork();
-			if (!pid) {
-				close(fd[0]);
-				dup2(fd[1], STDOUT_FILENO);
-				execve(cmd.av[0], cmd.av, env);
-			}
-			close(fd[1]);
-			if (fd[0]);
-				dup2(fd[0], STDIN_FILENO);
-			free(cmd.av);
-		} else {
-			int	i = -1;
-
-			cmd.ac = -1;
-			while (av[++cmd.ac]) {
-				if (!strncmp(av[cmd.ac], ";", 2) || !strncmp(av[cmd.ac], "|", 2))
-					break;
-			}
-			cmd.av = (char **)malloc(sizeof(char *) * (cmd.ac + 1));
-			cmd.av[cmd.ac] = NULL;
-			while (++i < cmd.ac)
-				cmd.av[i] = av[i];
-			av += cmd.ac;
-		}
+	cmd = token->cmd;
+	if (!strncmp(cmd->av[0], "cd", 3)) {
+		cd(cmd, token->env);
+		return ;
 	}
-	wait(0);
+	i = -1;
+	while (++i < token->size) {
+		if (pipe(fd) < 0) {
+			write(2, "error: fatal\n", 13);
+			exit(EXIT_FAILURE);
+		}
+		pid = fork();
+		if (pid < 0) {
+			write(2, "error: fatal\n", 13);
+			exit(EXIT_FAILURE);
+		}
+		if (!pid) {
+			if (cmd->next)
+				dup2(fd[1], STDOUT_FILENO);
+			close(fd[0]);
+			close(fd[1]);
+			if (execve(cmd->av[0], cmd->av, token->env) < 0) {
+				write(2, "error: cannot execute ", 22);
+				write(2, cmd->av[0], strlen(cmd->av[0]));
+				write(2, "\n", 1);
+				exit(EXIT_FAILURE);
+			}
+		}
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		cmd = cmd->next;
+	}
+	wait(NULL);
+}
 
-	return 0;
+int main(int ac, char** av, char** env) {
+	t_token	token;
+
+	(void)ac;
+	++av;
+	while (*av) {
+		memset(&token, 0, sizeof(t_token));
+		token.env = env;
+
+		av += parse(av, env, &token);
+		if (token.size)
+			exec(&token);
+		if (*av && !strncmp(*av, ";", 2))
+			av++;
+	}
 }
