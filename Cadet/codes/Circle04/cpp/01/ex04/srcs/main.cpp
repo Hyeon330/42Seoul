b7264beb2b6f778b9966a26ecfefa24480953b5b
Hyeonsu Lee <hyeonsul@c5r3s7.42seoul.kr>
2023-12-27 18:59:37 +0900

    update

--- Cadet/codes/Circle04/cpp/01/ex04/srcs/main.cpp
+++ Cadet/codes/Circle04/cpp/01/ex04/srcs/main.cpp
@@ -1,25 +1,6 @@
 #include <iostream>
 #include <fstream>
 
-// static void	replaceFile(std::ifstream& inputFile, std::ofstream& outputFile, std::string& s1, std::string& s2) {
-// 	std::string	line;
-
-// 	while (getline(inputFile, line)) {
-// 		size_t	pos = 0;	// position
-
-// 		while ((pos = line.find(s1, pos)) != std::string::npos) {
-// 			line.erase(pos, s1.length());	// s1을 삭제
-// 			line.insert(pos, s2);	// s2를 삽입
-// 			pos += s2.length();
-// 		}
-// 		outputFile << line;
-// 		if (inputFile.peek() != EOF)
-// 			outputFile << '\n';
-// 	}
-
-// 	std::cout << "Success!!" << std::endl;
-// }
-
 int main(int ac, char **av) {
 	if (ac != 4) {
 		std::cerr << "Arguments Error.";
@@ -29,36 +10,33 @@ int main(int ac, char **av) {
 	std::string	filename = av[1];
 	std::string	s1 = av[2];
 	std::string	s2 = av[3];
+	std::string	line;
 
 	std::ifstream	inputFile(filename);
 	if (!inputFile.is_open()) {
 		std::cerr << "Open Error.";
 		return 1;
 	}
-
 	std::ofstream	outputFile(filename + ".replace");
-	if (outputFile.is_open()) {
-		std::string	line;
-
-		while (getline(inputFile, line)) {
-			size_t	pos = 0;	// position
-
-			while ((pos = line.find(s1, pos)) != std::string::npos) {
-				line.erase(pos, s1.length());	// s1을 삭제
-				line.insert(pos, s2);	// s2를 삽입
-				pos += s2.length();
-			}
-			outputFile << line;
-			if (inputFile.peek() != EOF)
-				outputFile << '\n';
-		}
-
-		std::cout << "Success!!" << std::endl;
-	}
-	else {
+	if (!outputFile.is_open()) {
 		std::cerr << "Cannot Create File.";
 		return 1;
 	}
+
+	while (getline(inputFile, line)) {
+		size_t	pos = 0;	// position
+
+		while ((pos = line.find(s1, pos)) != std::string::npos) {
+			line.erase(pos, s1.length());	// s1을 삭제
+			line.insert(pos, s2);	// s2를 삽입
+			pos += s2.length();
+		}
+		outputFile << line;
+		if (inputFile.peek() != EOF)
+			outputFile << '\n';
+	}
+
+	std::cout << "Success!!" << std::endl;
 
 	return 0;
 }
