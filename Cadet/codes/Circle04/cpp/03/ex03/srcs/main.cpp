e00005a025358ed826845c67f052cc2f83bb38ce
Hyeonsu Lee <hyeonsul@c5r5s7.42seoul.kr>
2024-01-05 15:39:26 +0900

    udpate

--- Cadet/codes/Circle04/cpp/03/ex03/srcs/main.cpp
+++ Cadet/codes/Circle04/cpp/03/ex03/srcs/main.cpp
@@ -1,27 +1,39 @@
 #include "DiamondTrap.hpp"
 
 int main() {
-	DiamondTrap a("A");
-	DiamondTrap b("B");
+	DiamondTrap A("A");
+	DiamondTrap B("B");
 
-	std::cout << "-----------------------------------------------------------" << std::endl;
-	a.attack("B");
-	b.takeDamage(a.getAttackDamage());
-	b.beRepaired(3);
-	std::cout << "-----------------------------------------------------------" << std::endl;
-	b.attack("A");
-	a.takeDamage(b.getAttackDamage());
-	a.beRepaired(55);
-	std::cout << "-----------------------------------------------------------" << std::endl;
-	a.guardGate();
-	std::cout << "-----------------------------------------------------------" << std::endl;
-	b.highFivesGuys();
-	std::cout << "-----------------------------------------------------------" << std::endl;
-	a.whoAmI();
-	b.whoAmI();
-	std::cout << "-----------------------------------------------------------" << std::endl;
-	DiamondTrap c(a);
-	DiamondTrap d;
-	d = a;
-	std::cout << "-----------------------------------------------------------" << std::endl;
+	std::cout << std::endl;
+
+	A.attack("B");
+	B.takeDamage(A.getAttackDamage());
+	B.beRepaired(20);
+
+	std::cout << std::endl;
+
+	B.attack("A");
+	A.takeDamage(B.getAttackDamage());
+	A.beRepaired(40);
+
+	std::cout << std::endl;
+
+	A.guardGate();
+
+	std::cout << std::endl;
+
+	B.highFivesGuys();
+
+	std::cout << std::endl;
+
+	A.whoAmI();
+	B.whoAmI();
+
+	std::cout << std::endl;
+
+	DiamondTrap C(A);
+	DiamondTrap D;
+	D = A;
+	
+	std::cout << std::endl;
 }
